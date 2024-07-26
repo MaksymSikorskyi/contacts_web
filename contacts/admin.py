24d5368329c182ec0c2779e67214ecc8b76fadab
MaksymSikorskyi <maksim.sikorskyj@gmail.com>
2024-07-26 12:40:08 +0300

    Creation of Contact model and AdminContact model

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+
+    # to show atributes of the class on admin interfase
+    list_display = ('pk', 'email', 'name', 'phone', 'has_address')
+
+    # to register atributes for search on admin interface
+    search_fields = ('email', 'name', 'phone')
+
+    # to make an edit link of class atribute on admin interface
+    list_display_links = ('email', 'name')
+
+    # ordering of items displayed in admin interface
+    ordering = ('name',)
+
+    # decorator to show boolean value graphicaly
+    @admin.display(boolean=True)
+    # method to display result on admin interfase
+    def has_address(self, obj: 'Contact'):
+        return obj.address is not None and len(obj.address.strip()) > 0
--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=15)
+    address = models.TextField(blank=True, null=True, default=None)
+    notes = models.TextField(blank=True, null=True, default=None)
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return "<Contact id={} email={} phone={}>".format(
+            self.pk, self.email, self.name, self.phone
+        )
