4e98b61850414b5ae8711df36aaf9a01417e82e7
MaksymSikorskyi <maksim.sikorskyj@gmail.com>
2024-08-20 16:41:24 +0300

    Social provider google

--- contacts_web/settings.py
+++ contacts_web/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     # dev apps
     'django_extensions',
     "debug_toolbar",
